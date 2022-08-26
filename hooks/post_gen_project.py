d470d94aa5d4e948c9daa9df0f340ee08bed13f3
Andre F. Rendeiro <afrendeiro@gmail.com>
2022-08-26 12:28:15 +0200

    add a post gen hook

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,96 @@
+"""This module is called after project is created."""
+
+import textwrap
+from pathlib import Path
+
+PROJECT_DIRECTORY = Path.cwd().absolute()
+GITHUB_ORG = "rendeirolab"
+PROJECT_SLUG = "{{ cookiecutter.project_slug }}"
+PROJECT_NAME = "{{ cookiecutter.project_name }}"
+
+
+def main() -> None:
+    print_further_instuctions()
+
+
+def print_further_instuctions() -> None:
+    """Show user what to do next after project creation."""
+    cmds0 = "\n\t$ ".join(get_git_init_commands())
+    cmds1 = "\n\t$ ".join(get_additional_commands())
+    message = f"""
+    Your project '{PROJECT_NAME}' is created.
+
+    You should have registered your project on the lab's register:
+    https://cemmat.sharepoint.com/:x:/r/sites/rendeirolab/_layouts/15/Doc.aspx?sourcedoc=%7B4c72f84b-f33b-4162-a5e8-f05556fdf66b%7D
+
+    You can now start working on it, but there are a few additional things to do:
+
+    1) Start a github repository in the new directory structure:
+        $ cd {PROJECT_SLUG} && git init
+    2) Go to https://github.com/new and create a project with the same slug name: '{PROJECT_SLUG}'
+    3) Upload initial code to GitHub:
+        $ git add .
+        $ git commit -m "Initial commit"
+        $ git branch -M main
+        $ git remote add origin https://github.com/{GITHUB_ORG}/{PROJECT_SLUG}.git
+        $ git push -u origin main
+    4. Create a directory for the project in the CeMM cluster:
+        $ mkdir -p /research/groups/lab_rendeiro/projects/{PROJECT_SLUG}/data
+    5. Create a directory for the project in the CeMM cluster at:
+        $ mkdir -p /nobackup/groups/lab_rendeiro/projects/{PROJECT_SLUG}/
+    6. Create a soft link between '/research/.../data' and '/nobackup/.../data':
+        $ cd /nobackup/groups/lab_rendeiro/projects/{PROJECT_SLUG}/
+        $ ln -s /research/groups/lab_rendeiro/projects/{PROJECT_SLUG}/data ./
+    6. Create a 'cemm_metadata.json' file in '/research/groups/lab_rendeiro/projects/{PROJECT_SLUG}/'
+    7. Create a 'cemm_metadata.json' file in '/nobackup/groups/lab_rendeiro/projects/{PROJECT_SLUG}/'
+
+    Summary of next steps:
+        $ {cmds0}
+        $ {cmds1}
+
+    Don't forget also to describe the project in 'cemm_metadata.json' files as well.
+    """
+    print(textwrap.dedent(message))
+
+
+def get_git_init_commands():
+
+    cmds = [
+        f"cd {PROJECT_SLUG}",
+        "git init",
+        "git add .",
+        'git commit -m "Initial commit"',
+        "git branch -M main",
+        f"git remote add origin https://github.com/{GITHUB_ORG}/{PROJECT_SLUG}.git",
+        "git push -u origin main",
+    ]
+    return cmds
+
+
+def get_additional_commands():
+
+    cmds = [
+        "ssh {{ cookiecutter.username }}@login",
+        f"mkdir -p /research/groups/lab_rendeiro/projects/{PROJECT_SLUG}/data",
+        f"mkdir -p /nobackup/groups/lab_rendeiro/projects/{PROJECT_SLUG}/",
+        f"cd /nobackup/groups/lab_rendeiro/projects/{PROJECT_SLUG}/",
+        f"ln -s /research/groups/lab_rendeiro/projects/{PROJECT_SLUG}/data ./",
+    ]
+    return cmds
+
+
+def init_git_repo() -> None:
+    """
+    Create git repository and add to remote.
+
+    Unfortunately it doesn't work as it would require the template instance to already exist but that will only happen after this script finishes.
+    """
+    import os
+
+    cmd = " && ".join(get_git_init_commands())
+    assert Path(PROJECT_SLUG).exists(), "Directory was not created yet!"
+    os.system(cmd)
+
+
+if __name__ == "__main__":
+    main()
